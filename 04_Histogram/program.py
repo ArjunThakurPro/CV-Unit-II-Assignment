ea6885aabbea59ec91f09c6cd03f813c85665947
Arjun Thakur <arjn6397@gmail.com>
2026-09-13 07:07:34 +0530

    Add files via upload

--- 04_Histogram/program.py
+++ 04_Histogram/program.py
@@ -1,25 +1,9 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 
-img = cv2.imread("input.jpg", cv2.IMREAD_GRAYSCALE)
-if img is None:
-    print("Image not found!")
-    exit()
-
-hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-
-highest_intensity = np.argmax(hist)
-
-print("Intensity with highest frequency:", highest_intensity)
-
-plt.figure(figsize=(10, 5))
-plt.plot(hist)
-plt.title("Grayscale Image Histogram")
-plt.xlabel("Intensity")
-plt.ylabel("Frequency")
-plt.xlim([0, 256])
-
-plt.savefig("output.png")
-
-plt.show()
+img = cv2.imread("input.jpg", 0)
+min = np.min(img)
+max = np.max(img)
+st = (img - min) * (255.0 / (max - min))
+st = st.astype(np.uint8)
+cv2.imwrite("output.png", st)
